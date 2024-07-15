96a5bd70a0008d5e4a360cc244e4d72c33a4135e
lulinhu <lulinhu@hisense.com>
2024-07-15 19:50:35 +0800

    commit complete codes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use anyhow::{ anyhow, Result };
 use clap::Parser;
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{ header, Client, Response, Url };
+use std::{ collections::HashMap, str::FromStr };
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{ Style, ThemeSet },
+    parsing::SyntaxSet,
+    util::{ as_24_bit_terminal_escaped, LinesWithEndings },
+};
 
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", author = "tiger_lu")]
@@ -13,16 +24,137 @@ enum SubCommand {
     Post(Post),
 }
 
+#[derive(Parser, Debug)]
 struct Get {
-    #[clap(short, long)]
-    id: Option<String>,
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
 }
 
+#[derive(Parser, Debug)]
 struct Post {
-    #[clap(short, long)]
-    id: Option<String>,
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn parse_url(s: &str) -> Result<String> {
+    // 这里我们仅仅检查一下 URL 是否合法
+    let _url: Url = s.parse()?;
+
+    Ok(s.into())
+}
+
+#[derive(Debug, PartialEq)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: split.next().ok_or_else(err)?.to_string(),
+            v: split.next().ok_or_else(err)?.to_string(),
+        })
+    }
+}
+
+/// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    s.parse()
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+
+    let cli = reqwest::Client::builder().default_headers(headers).build()?;
+
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(cli, args).await?,
+        SubCommand::Post(ref args) => post(cli, args).await?,
+    };
+
+    Ok(result)
+}
+
+async fn get(cli: Client, args: &Get) -> Result<()> {
+    let resp = cli.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+/// 处理 post 子命令
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    println!();
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+/// 打印整个响应
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+/// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h: HighlightLines = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight_line(line, &ps).unwrap();
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
 }
